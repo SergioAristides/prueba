dfcaccc00459e103df67187320988f6c311696e4
SergioAristides <sergioa.riverab@autonoma.edu.co>
2022-09-21 11:44:01 -0500

    primero

---
+++ main.py
@@ -0,0 +1,33 @@
+def main():
+    {
+"DueDate": "2022-02-24",
+"Balance": 1990.19,
+"DocNumber": "1053811422",
+"Status": "Payable",
+"Line": [
+{
+"Description": "Sample Expense",
+"Amount": 500,
+"DetailType": "ExpenseDetail",
+"ExpenseDetail": {
+"Customer": {
+"value": "ABC123",
+"name": "Sample Customer",
+"Ref": {
+"value": "DEF234",
+"name": "Sample Construction",
+"Address": {
+"city": "Manizales",
+"code_zip": "170002, 170001, 170003, 170004",
+}
+}
+},
+"LineStatus": "Billable"
+}
+}
+],
+"TotalAmt": 1990.19
+}
+
+if __name__=='__main__':
+    main()
